49c06e09a903aec10f05b8b1d23dd06c6c66349b
ellianw <elliangwolfart@gmail.com>
2025-06-03 22:43:48 -0300

    Added Sales panels and controllers

---
+++ src/main/java/Controllers/SaleController.java
@@ -0,0 +1,99 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controllers;
+
+import DAO.SaleDAO;
+import Views.JpnSales;
+import java.sql.Connection;
+import javax.swing.table.DefaultTableModel;
+
+/**
+ *
+ * @author Ellian
+ */
+public class SaleController {
+    private SaleDAO dao = null;
+    private JpnSales panel = null;
+
+    public SaleController(Connection conn) {
+        dao = new SaleDAO(conn);
+    }
+
+    public SaleController() {
+    }    
+    
+    public void setPanel(JpnSales panel) {
+        this.panel = panel;
+    }
+    
+    public DefaultTableModel getFilledTableModel() {
+        String[] colunas = { "ID", "Nome", "Descrição", "Valor","Estoque" };
+        DefaultTableModel tableModel = new DefaultTableModel(colunas,0);
+//        List<Client> clientList = null;
+//        
+//        try {
+//            clientList = dao.findAll();
+//        } catch (Exception e) {
+//            System.out.println("Erro ao buscar clientes: "+e);
+//        }
+//
+//        if (clientList == null ) return tableModel;
+//        
+//        for (Client s : clientList) {
+//            Object[] linha = {
+//                s.getId(),
+//                s.getName(),
+//                s.getEmail(),
+//                s.getPhone(),
+//                s.getCPF()
+//            };
+//            tableModel.addRow(linha);
+//        }
+        
+        return tableModel;
+    }
+    
+    public boolean saveSale() {
+       
+//        Client editingClient = new Client(panel.getClientId(),
+//                panel.getClientName().getText().trim(),
+//                panel.getClientEmail().getText().trim(),
+//                panel.getClientPhone().getText().trim(),
+//                panel.getClientCPF().getText().trim());
+//        try {
+//            if (editingClient.getId() == null) {
+//                dao.insert(editingClient);
+//            } else {
+//                dao.update(editingClient);
+//            }
+//        } catch (Exception e) {
+//            System.out.println("SQL error while inserting or updating clients: "+e);
+//            return false;
+//        }
+        
+        return true;
+    }
+//    NEED REFACTOR
+    public boolean deleteSale(Integer id) {
+//        try {
+//            dao.delete(id);
+//        } catch (Exception e) {
+//            System.out.println("SQL error while deleting client: "+e);
+//            return false;
+//        }
+        return true;
+    }
+    
+    public boolean editSale(Integer id) {
+//        try {
+//            Product product = dao.findById(id);
+//            panel.setEditingProduct(product);
+//        } catch (Exception e) {
+//            System.out.println("SQL error while editing client: "+e);
+//            return false;
+//        }
+        return true;
+    }
+}
